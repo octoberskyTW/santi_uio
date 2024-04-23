0e54efc05c0cf238d639e71fc86bde77360168ce
Dung-Ru Tsai <you@example.com>
2024-04-22 19:23:30 -0700

    Add coding style scripts

---
+++ README.md
@@ -0,0 +1,8 @@
+# SANTI_UIO
+
+## Coding Style
+Use clang-format to fix the coding style
+```
+cd santi_uio
+clang-format -i src/*.c
+```
--- src/santi_uio_main.c
+++ src/santi_uio_main.c
@@ -1,14 +1,14 @@
-#include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/uio_driver.h>
-#include <linux/platform_device.h>
+#include <linux/module.h>
 #include <linux/of.h>
 #include <linux/of_address.h>
+#include <linux/platform_device.h>
 #include <linux/slab.h>
+#include <linux/uio_driver.h>
 
 
 struct santi_uio_ctrlblk_t {
-	void __iomem *base;
+    void __iomem *base;
     struct uio_info uinfo;
 };
 
@@ -17,51 +17,52 @@ static struct santi_uio_ctrlblk_t su_blk = {0};
 static int santi_uio_probe(struct platform_device *pdev)
 {
     // Device initialization and registration with UIO
-	int ret = 0;
+    int ret = 0;
     struct device_node *fe_mem = NULL;
     struct santi_uio_ctrlblk_t *su = &su_blk;
     struct uio_info *info = &su->uinfo;
-	struct resource res;
+    struct resource res;
 
     pr_info("Entering %s\n", __func__);
 
-	fe_mem = of_parse_phandle(pdev->dev.of_node, "fe_mem", 0);
-	if (!fe_mem) {
-		pr_err("%s: can not find fe_mem node failed\n", __FUNCTION__);
-		return -ENODEV;
-	}
-	if (of_address_to_resource(fe_mem, 0, &res)) {
-        pr_err("%s: of_address_to_resource failed\n", __FUNCTION__);
-		return -ENXIO;
+    fe_mem = of_parse_phandle(pdev->dev.of_node, "fe_mem", 0);
+    if (!fe_mem) {
+        pr_err("%s: can not find fe_mem node failed\n", __FUNCTION__);
+        return -ENODEV;
     }
-	/* map fe_mem */
-	//su->base = devm_ioremap_resource(&pdev->dev, &res);
+    if (of_address_to_resource(fe_mem, 0, &res)) {
+        pr_err("%s: of_address_to_resource failed\n", __FUNCTION__);
+        return -ENXIO;
+    }
+    /* Mapping the Virtual Memory */
     su->base = devm_ioremap(&pdev->dev, res.start, resource_size(&res));
-	if (IS_ERR(su->base)) {
-        pr_err("%s: devm_ioremap_resource failed\n", __FUNCTION__);
+    if (IS_ERR(su->base)) {
+        pr_err("%s: devm_ioremap failed\n", __FUNCTION__);
         return PTR_ERR(su->base);
     }
 
-	of_node_put(fe_mem);
+    of_node_put(fe_mem);
 
     info->name = "santi_uio_device";
-	info->version = "0.0.1";
-	info->mem[0].internal_addr = su->base;
-    pr_info("%s: Virtual Address 0x%llx\n", __FUNCTION__, info->mem[0].internal_addr);
+    info->version = "0.0.1";
+    info->mem[0].internal_addr = su->base;
+    pr_info("%s: Virtual Address 0x%llx\n", __FUNCTION__,
+            info->mem[0].internal_addr);
     info->mem[0].addr = res.start;
     pr_info("%s: Physical Address 0x%llx\n", __FUNCTION__, info->mem[0].addr);
-	info->mem[0].size = resource_size(&res);
-	info->mem[0].memtype = UIO_MEM_PHYS;
+    info->mem[0].size = resource_size(&res);
+    info->mem[0].memtype = UIO_MEM_PHYS;
 
     ret = uio_register_device(&pdev->dev, info);
-	 if (ret) {
-        pr_err("%s: uio_register_device failed: %p\n", __FUNCTION__, ERR_PTR(ret));
-		goto err_uio_register_device;
-     }
+    if (ret) {
+        pr_err("%s: uio_register_device failed: %p\n", __FUNCTION__,
+               ERR_PTR(ret));
+        goto err_uio_register_device;
+    }
     pr_info("%s: SANTI PROBE P.A.S.S\n", __FUNCTION__);
     return 0;
 err_uio_register_device:
-	return ret;
+    return ret;
 }
 
 static int santi_uio_remove(struct platform_device *pdev)
@@ -73,25 +74,22 @@ static int santi_uio_remove(struct platform_device *pdev)
     return 0;
 }
 
-// static struct uio_info santi_uio_info = {
-//     .name = "SantiUIODriver",
-//     .version = "0.1",
-//     // Fill in other fields as necessary
-// };
-
 static const struct of_device_id of_santi_match[] = {
-	{ .compatible = "mediatek,pce", },
-	{ },
+    {
+        .compatible = "mediatek,pce",
+    },
+    {},
 };
 
 static struct platform_driver santi_uio_driver = {
     .probe = santi_uio_probe,
     .remove = santi_uio_remove,
-	.driver = {
-		.name = "SantiUIODriver",
-		.owner = THIS_MODULE,
-		.of_match_table = of_santi_match,
-	},
+    .driver =
+        {
+            .name = "SantiUIODriver",
+            .owner = THIS_MODULE,
+            .of_match_table = of_santi_match,
+        },
 };
 MODULE_DEVICE_TABLE(of, of_santi_match);
 
